3582c7dc38d85ed711afd6a173f8f6adb8db176d
epmik <steven.kimpe@tothedot.be>
2025-02-02 14:45:58 +0100

    Vector3 Unit Tests

--- Rasterizer.UnitTests/Vector3UnitTests.cs
+++ Rasterizer.UnitTests/Vector3UnitTests.cs
@@ -10,30 +10,72 @@ namespace Rasterizer.UnitTests
         }
 
         [Test]
-        public void NewVector3EqualsZero()
+        public void EmptyDefaultConstructorTest()
         {
             var a = new Vector3();
 
-            var b = new Vector3();
+            var expected = new Vector3(0.0f, 0.0f, 0.0f);
 
-            b.X = 0;
-            b.Y = 0;
-
-            Assert.IsTrue(EqualsFuzzy(a, b));
+            Assert.IsTrue(EqualsFuzzy(a, expected));
         }
 
         [Test]
-        public void Vector3Addition()
+        public void DefaultConstructorTest()
         {
-            var a = new Vector3(0.0f, 0.0f);
+            var expected = new Vector3(8.265f, 4.5978f, 0.554f);
 
-            var b = new Vector3(2.0f, 1.0f);
+            var a = new Vector3(expected.X, expected.Y, expected.Z);
 
-            var expected = new Vector3(2.0f, 1.0f);
+            Assert.IsTrue(EqualsFuzzy(a, expected));
+        }
 
-            Assert.IsTrue(EqualsFuzzy(expected, a + b));
+        [Test]
+        public void Vector3AddTest()
+        {
+            var otkA = new OpenTK.Mathematics.Vector3(0.0f, 0.0f, 0.0f);
+            var otkB = new OpenTK.Mathematics.Vector3(8.1f, 2.14f, 8.9835f);
 
+            var expected = OpenTK.Mathematics.Vector3.Add(otkA, otkB);
 
+            Assert.IsTrue(EqualsFuzzy(expected, new Vector3(otkA.X, otkA.Y, otkA.Z) + new Vector3(otkB.X, otkB.Y, otkB.Z)));
+        }
+
+        [Test]
+        public void Vector3SubtractTest()
+        {
+            var otkA = new OpenTK.Mathematics.Vector3(0.0f, 0.0f, 0.0f);
+            var otkB = new OpenTK.Mathematics.Vector3(8.1f, 2.14f, 8.9835f);
+
+            var expected = OpenTK.Mathematics.Vector3.Subtract(otkA, otkB);
+
+            Assert.IsTrue(EqualsFuzzy(expected, new Vector3(otkA.X, otkA.Y, otkA.Z) - new Vector3(otkB.X, otkB.Y, otkB.Z)));
+        }
+
+        [Test]
+        public void Vector3MultiplyTest()
+        {
+            var otkA = new OpenTK.Mathematics.Vector3(2.1f, 1.78f, 4.89f);
+            var otkB = new OpenTK.Mathematics.Vector3(8.1f, 2.14f, 8.9835f);
+
+            var expected = OpenTK.Mathematics.Vector3.Multiply(otkA, otkB);
+
+            Assert.IsTrue(EqualsFuzzy(expected, new Vector3(otkA.X, otkA.Y, otkA.Z) * new Vector3(otkB.X, otkB.Y, otkB.Z)));
+        }
+
+        [Test]
+        public void Vector3DivideTest()
+        {
+            var otkA = new OpenTK.Mathematics.Vector3(0.0f, 0.0f, 0.0f);
+            var otkB = new OpenTK.Mathematics.Vector3(8.1f, 2.14f, 8.9835f);
+
+            var expected = OpenTK.Mathematics.Vector3.Divide(otkA, otkB);
+
+            Assert.IsTrue(EqualsFuzzy(expected, new Vector3(otkA.X, otkA.Y, otkA.Z) / new Vector3(otkB.X, otkB.Y, otkB.Z)));
+        }
+
+        private bool EqualsFuzzy(OpenTK.Mathematics.Vector3 a, Vector3 b, float fuzzy = 0.0001f)
+        {
+            return EqualsFuzzy(a.X, b.X, fuzzy) && EqualsFuzzy(a.Y, b.Y, fuzzy) && EqualsFuzzy(a.Z, b.Z, fuzzy);
         }
 
         private bool EqualsFuzzy(Vector3 a, Vector3 b, float fuzzy = 0.0001f)
