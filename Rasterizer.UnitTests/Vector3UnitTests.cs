1aa85ba6be8328236c2a8f961b1fa5a9203295a3
epmik <steven.kimpe@tothedot.be>
2025-02-02 10:50:01 +0100

    development

---
+++ Rasterizer.UnitTests/Vector3UnitTests.cs
@@ -0,0 +1,16 @@
+namespace Rasterizer.UnitTests
+{
+    public class Vector3UnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
